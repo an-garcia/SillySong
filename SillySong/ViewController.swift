64599f7d25d96d038e9eaf7c57cc1851e42e11f5
Angel Newton <xanewton@gmail.com>
2017-10-01 20:00:23 -0400

    Add Connections to the ViewControler

--- SillySong/ViewController.swift
+++ SillySong/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameField: UITextField!
+    
+    @IBOutlet weak var liricsField: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func reset(_ sender: Any) {
+    }
+    
+    @IBAction func displayLyrics(_ sender: Any) {
+    }
 }
 
